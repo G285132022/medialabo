ac0a93e6936fca4344528af8158316874fdababb
G285132022 <228513@st.takushoku-u.ac.jp>
2023-07-20 15:03:17 +0900

    0720

--- weather.js
+++ weather.js
@@ -46,4 +46,65 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
-
+let b = document.querySelector('button#botan');
+  b.addEventListener('click',result);
+  
+ /* function result() {
+    let a = document.querySelector('select#kensaku');
+    let an = a.selectedIndex;
+    let c = a.querySelectorAll('option');
+    let cn = c.item(an);
+    let f = cn.getAttribute('value');
+    return {
+      f
+    };
+  }
+  b.addEventListener('click',sendRequest);
+  
+  function sendRequest() {
+    let e = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + result().f + '.json';
+    let url = e;
+    axios.get(url)
+          .then(showResult)   
+          .catch(showError)  
+          .then(finish);
+  }
+  function showResult(resp) {
+    let data = resp.data;
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+    console.log(data);
+    console.log(data.x);
+    for (n of data.weather) {
+      console.log(n);
+      var obj = n; 
+    }
+    
+    let t1 = document.querySelector('span#name');
+    t1.textContent = data.name;
+    let t2 = document.querySelector('span#ido');
+    t2.textContent = data.coord.lat;
+    let t3 = document.querySelector('span#keido');
+    t3.textContent = data.coord.lon;
+    let t4 = document.querySelector('span#weather');
+    t4.textContent = obj.description;
+    let t5 = document.querySelector('span#mintemp');
+    t5.textContent = data.main.temp_min;
+    let t6 = document.querySelector('span#maxtemp');
+    t6.textContent = data.main.temp_max;
+    let t7 = document.querySelector('span#situdo');
+    t7.textContent = data.main.humidity;
+    let t8 = document.querySelector('span#husoku');
+    t8.textContent = data.wind.speed;
+    let t9 = document.querySelector('span#hukou');
+    t9.textContent = data.wind.deg;
+  }
+  function showError(err) {
+    console.log(err);
+  }
+  function finish() {
+    console.log('Ajax 通信が終わりました');
+  }
+  */
+ 
